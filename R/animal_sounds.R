3dae5d4e28cdc3c0eac36e5b243d6e17235ea5ad
ihsanfadil <ifadilah@oucru.org>
2025-06-29 22:10:23 +0700

    added some more details to the documentation file.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,18 @@
+
+#' Specifies an animal and its sound.
+#'
+#' This function uses a [paste0] from _base_ R.
+#'
+#' @param animal
+#' @param sound
+#'
+#' @returns The name of animal and its sound.
+#' @export
+#'
+#' @examples animal_sounds("cat", "purrr")
+
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " goes ", sound, "!")
+  paste0("The ", animal, " says ", sound, " ", sound, "!")
 }
