28f615f252b8a3dbe08ff5aa3f5ab970a54dff9d
ihsanfadil <ifadilah@oucru.org>
2025-06-29 22:47:10 +0700

    Add imports

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -3,16 +3,23 @@
 #'
 #' This function uses a [paste0] from _base_ R.
 #'
-#' @param animal
-#' @param sound
+#' @param animal Name of the animal
+#' @param sound Sound of the animal
 #'
 #' @returns The name of animal and its sound.
 #' @export
 #'
 #' @examples animal_sounds("cat", "purrr")
 
+# animal_sounds <- function(animal, sound) {
+#   stopifnot(is.character(animal) & length(animal) == 1)
+#   stopifnot(is.character(sound) & length(sound) == 1)
+#   paste0("The ", animal, " says ", sound, " ", sound, "!")
+# }
+
 animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " says ", sound, " ", sound, "!")
+  if (!rlang::is_character(animal) || !rlang::is_character(sound)) {
+    cli::cli_abort("Both 'animal' and 'sound' must be character strings.")
+  }
+  paste(animal, "says", sound)
 }
